7c06e0b6e0e5af59c84bb8d2a69055c50b8de90a
Alena2110 <90158282+Alena2110@users.noreply.github.com>
2022-04-20 17:49:29 +0300

    л

--- src/Triangle.java
+++ src/Triangle.java
@@ -6,11 +6,11 @@ public class Triangle {
     private double a2;//координаты точки a по оси y
     private double b1;//координаты точки b по оси x
     private double b2;//координаты точки b по оси y
-    private double c1;//координаты точки b по оси x
-    private double c2;//координаты точки b по оси y
-    private double AB;
-    private double BC;
-    private double AC;
+    private double c1;//координаты точки с по оси x
+    private double c2;//координаты точки с по оси y
+    private double AB;//длина стороны AB
+    private double BC;//длина стороны BC
+    private double AC;//длина стороны AC
 
     Triangle(double a1, double a2, double b1, double b2, double c1, double c2) {
         this.a1 = a1;
@@ -24,16 +24,16 @@ public class Triangle {
         this.AC = Math.pow((Math.pow((this.a1 - this.c1), 2) + Math.pow((this.a2 - this.c2), 2)), 0.5);
     }
 
-    public double perimeter() {
+    public double perimeter() { //периметр треугольника
         return (this.AB + this.BC + this.AC);
     }
 
-    public double square() {
+    public double square() { //площадь треугольника
         double p = perimeter() / 2;
         return Math.pow((p * (p - this.AB) * (p - this.BC) * (p - this.AC)), 0.5);
     }
 
-    public String medianIntersection() {
+    public String medianIntersection() {//точка пересечения медиан треугольника
         return "(" + Math.round((this.a1 + this.b1 + this.c1) / 3 * 10.0) / 10.0 + ":" +
                 Math.round((this.a2 + this.b2 + this.c2) / 3 * 10.0) / 10.0 + ")";
     }
